2bc22f6deb21d024b940162fec02f22c82f22f47
admin <promitay@gmail.com>
2022-12-19 23:44:29 +0300

    Создание главного меня и настройка нажатия кнопок в нем.

--- src/main/java/com/example/animalsheltertelegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/animalsheltertelegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,9 +2,10 @@ package com.example.animalsheltertelegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.*;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +16,8 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    private TelegramBot telegramBot;
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private  final TelegramBot telegramBot;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
@@ -30,28 +31,56 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-
             logger.info("Processing update: {}", update);
-            long chatId = update.message().chat().id();
-            String receivedMessage = update.message().text();
-            String username = update.message().chat().username();
-            String firstName = update.message().chat().firstName();
-            String lastName = update.message().chat().lastName();
-
-            if (receivedMessage.equals("/start")) {
-                mailing(chatId,
-                        "Добрый день. Рады приветствовать Вас в нашем приюте.");
-            } else {
-                mailing(chatId, "Переадрисовываю Ваш вопрос менеджеру.");
+            //Вызов главного меню с помощью сообщения /start
+            if (update.message() != null && "/start".equals(update.message().text())) {
+                telegramBot.execute(mainMenu(update.message().chat().id()));
+            }
+            //Конфигурирование нажатия кнопок в главном меню
+            if (update.callbackQuery() != null) {
+                String data = update.callbackQuery().data();
+                telegramBot.execute(new SendMessage(update.callbackQuery().message().chat().id(), data));
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
     // Данный метод отпраляет сообщение пользователю
-    public void mailing(long chatId, String receivedMessage) {
-        logger.info("Отправка сообщения");
-        SendMessage message = new SendMessage(chatId, receivedMessage);
-        SendResponse response = telegramBot.execute(message);
+//    public void mailing(long chatId, String receivedMessage) {
+//        logger.info("Отправка сообщения");
+//        SendMessage message = new SendMessage(chatId, receivedMessage);
+//
+//        SendResponse response = telegramBot.execute(message);
+//    }
+
+    //Метод создает Главного меню
+    private SendMessage mainMenu(long chatId) {
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Узнать информацию о приюте").callbackData("1");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Как взять собаку из приюта").callbackData("2");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Прислать отчет о питомце").callbackData("3");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Позвать волонтера").callbackData("4");
+        inlineKeyboard.addRow(button1);
+        inlineKeyboard.addRow(button2);
+        inlineKeyboard.addRow(button3);
+        inlineKeyboard.addRow(button4);
+        return new SendMessage(chatId, "Добрый день. Рады приветствовать Вас в нашем приюте.").replyMarkup(inlineKeyboard);
     }
+
+    //Метод создает меню - информация о приюте
+    private SendMessage infoMenu(long chatId) {
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Рассказать о приюте").callbackData("text1");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Выдать расписание работы приюта и адрес, схему проезда").callbackData("text2");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Выдать общие рекомендации о технике безопасности на территории приюта").callbackData("text3");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Принять и записать контактные данные для связи").callbackData("BD");
+        InlineKeyboardButton button5 = new InlineKeyboardButton("Позвать волонтера").callbackData("5");
+        inlineKeyboard.addRow(button1);
+        inlineKeyboard.addRow(button2);
+        inlineKeyboard.addRow(button3);
+        inlineKeyboard.addRow(button4);
+        inlineKeyboard.addRow(button5);
+        return new SendMessage(chatId, "Добро пожаловать в меню - Информация о приюте.").replyMarkup(inlineKeyboard);
+    }
+
 }
